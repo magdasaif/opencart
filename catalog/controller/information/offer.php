6b4f4acabcc2f5d294bd958b5764cabe16f125bc
magdasaif <magdasaif3@gmail.com>
2025-09-13 00:44:04 +0300

    create new custom offer page

---
+++ catalog/controller/information/offer.php
@@ -0,0 +1,43 @@
+<?php
+namespace Opencart\Catalog\Controller\Information;
+/**
+ * Class Offer
+ *
+ * @package Opencart\Catalog\Controller\Information
+ */
+class Offer extends \Opencart\System\Engine\Controller {
+	/**
+	 * Index
+	 *
+	 * @return void
+	 */
+	public function index(): void {
+		$this->load->language('information/offer');
+
+		$this->document->setTitle($this->language->get('heading_title'));
+
+		$data['breadcrumbs'] = [];
+
+		$data['breadcrumbs'][] = [
+			'text' => $this->language->get('text_home'),
+			'href' => $this->url->link('common/home', 'language=' . $this->config->get('config_language'))
+		];
+
+		$data['breadcrumbs'][] = [
+			'text' => $this->language->get('heading_title'),
+			'href' => $this->url->link('information/offer', 'language=' . $this->config->get('config_language'))
+		];
+
+		$data['description'] = $this->language->get('text_description');
+
+
+		$data['column_left'] = $this->load->controller('common/column_left');
+		$data['column_right'] = $this->load->controller('common/column_right');
+		$data['content_top'] = $this->load->controller('common/content_top');
+		$data['content_bottom'] = $this->load->controller('common/content_bottom');
+		$data['footer'] = $this->load->controller('common/footer');
+		$data['header'] = $this->load->controller('common/header');
+
+		$this->response->setOutput($this->load->view('information/offer', $data));
+	}
+}
---
+++ catalog/language/en-gb/information/offer.php
@@ -0,0 +1,4 @@
+<?php
+// Heading
+$_['heading_title']     = 'Offer';
+$_['text_description']  = 'Special offers are available on request. Please contact us for more details.';
